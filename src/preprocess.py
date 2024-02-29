712320cdf6ac2b6c3dfc361ae49881983d3de59a
Vaclav Korvas <korvasv@seznam.cz>
2024-02-29 17:34:36 +0100

    Redo whole code for preprocessing of the dataset

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,76 +1,70 @@
-import re
-import csv
-import argparse
+"""
+preprocess.py
+PDS Project - Log file analysis and anomaly detection
+Author: Václav Korvas (xkorva03)
+Module with data preprocessing and feature extraction
+"""
+
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
-log_file = '../logs/HDFS-test.log'
+class Preprocess:
 
-def parse_args():
-    parser = argparse.ArgumentParser("Preprocess the given HDFS log file.")
-    parser.add_argument('--log_file', type=str, help='The input log file')
-    parser.add_argument('--log_template_file', type=str, help='The input log template file')
-    parser.add_argument('--label_file', type=str, help='The input file with labels')
-    return parser.parse_args()
+    def __init__(self):
+        pass
 
-def read_log(log_file):
-    with open(log_file, 'r') as f:
-        log = f.readlines()
-        log = [l.strip() for l in log]
-    return log
+    def preprocess(self, df : pd.DataFrame) -> pd.DataFrame:
+        """
+        Preprocesses the input dataframe into a format suitable for machine learning
+        """
 
-def read_log_template_csv(log_template_file):
-    with open(log_template_file, 'r') as f:
-        log_template = list(csv.reader(f))
-    return log_template[1:]
+        columns_to_drop = ['Date', 'Time', 'PID', 'Content', 'Level', 'Component']
+        sorted_cols = ['E' + str(i) for i in range(1, 30)]
+        labels = df[['Label', 'BlockId']].drop_duplicates()
 
-def read_label_csv(label_file):
-    data = {}
-    with open(label_file, 'r') as f:
-        #label = list(csv.reader(f))
-        for row in csv.reader(f):
-            data[row[0]] = row[1]
-        data.pop('BlockId')
-    return data
+        df = df.drop(columns=columns_to_drop)
+        df = df.groupby(['BlockId', 'EventId']).size().unstack(fill_value=0).reset_index()
+        df = df[['BlockId'] + sorted_cols]
+        df = pd.merge(df, labels, on='BlockId')
+        df['Label'] = df['Label'].map({'Normal': 0, 'Anomaly': 1})
+        df = df.drop(columns=['BlockId'])
 
-def to_dataframe(events):
-    columns = ['Date', 'Time', 'PID', 'Level', 'Component', 'Content', 'EventId', 'BlockId', 'Label']
-    df = pd.DataFrame(events, columns=columns)
-    return df
+        return df
 
-def extract_blk_id(line):
-    blk_id = re.findall(r'blk_-?\d+', line)
-    if len(blk_id) == 0:
-        return None
-    return blk_id[0]
+    def normal_anomaly_ratio(self, df : pd.DataFrame, normal_ratio):
+        """
+        Balance the input dataframe by dropping some normal samples
+        """
 
-def extract_label(anotations, blk_id):
-    if blk_id in anotations:
-        return anotations[blk_id]
-    else:
-        return None
+        anomaly_cnt = len(df[df['Label'] == 1])
+        desired_normal_cnt = int((anomaly_cnt/ (1-normal_ratio)) - anomaly_cnt)
 
-def match_event(log, log_template, labels):
-    for l in log:
-        blk_id = extract_blk_id(l[-1])
-        label = extract_label(labels, blk_id)
-        event_id = None
-        for pat in log_template:
-            pattern = re.compile(pat[1])
-            match = pattern.match(l[-1])
-            if match:
-                event_id = pat[0]
-                break
-        if blk_id and match and label:
-            event = [event_id, blk_id, label]
-            l.extend(event)
-        else:
-            print("No match found for: ", l[-1])
-    return log 
+        normal = df[df['Label'] == 0]
+        normal = normal.sample(desired_normal_cnt)
+        df = pd.concat([normal, df[df['Label'] == 1]])
 
-log = read_log(log_file)
-log = [l.split(" ", 5) for l in log]
-log_template = read_log_template_csv('../logs/HDFS_templates.csv')
-labels = read_label_csv('../logs/anomaly_label.csv')
-events = match_event(log, log_template, labels)
-df = to_dataframe(events)
-print(df.head(10))
+        return df
+
+    def data_split(self, df : pd.DataFrame, test_size):
+        """
+        Split the input dataframe into train and test sets
+        """
+        labels = df['Label']
+        df = df.drop(columns=['Label'])
+        matrix = np.array(df.values.tolist())
+        train_data, test_data, train_labels, test_labels = train_test_split(matrix, labels, test_size=test_size, random_state=42)
+
+        return train_data, test_data, train_labels, test_labels
+        
+
+    def statistics(self, df : pd.DataFrame):
+        """
+        Computes statistics from the input dataframe
+        """
+        normal = df[df['Label'] == 0]
+        abnormal = df[df['Label'] == 1]
+        print("Data statistics:")
+        print("Total: ", len(df))
+        print("Normal: ", len(normal), " percentage: ", round(len(normal) / len(df), 3))
+        print("Anomaly: ", len(abnormal), " percentage: ", round(len(abnormal) / len(df),3))
